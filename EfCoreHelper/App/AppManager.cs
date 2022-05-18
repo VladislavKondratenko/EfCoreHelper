9ff48a177de25ee97dd5e4a70f80526555e5ecba
VladKondratenko <vladislavkondratenko119966@gmail.com>
2022-05-18 14:43:17 +0300

    sf

---
+++ EfCoreHelper/App/AppManager.cs
@@ -0,0 +1,82 @@
+﻿using EfCoreHelper.FilePart;
+
+namespace EfCoreHelper.App;
+
+public class AppManager
+{
+	private string _path = null!;
+
+	public void Run()
+	{
+		bool isWrongPath;
+
+		do
+		{
+			isWrongPath = TryAskPath();
+		} while (isWrongPath);
+
+		RunNewSession();
+
+		PrintStatistic();
+		PrintFinalMessages();
+	}
+
+	private void RunNewSession()
+	{
+		AppProcess.NewSession();
+		var fileContext = new FileManager(_path ?? throw new InvalidOperationException());
+		fileContext.DoWind();
+	}
+
+	private bool TryAskPath()
+	{
+		PrintAskMessage();
+		_path = Console.ReadLine()!;
+
+		var isWrongPath = string.IsNullOrWhiteSpace(_path) || Directory.Exists(_path) is not true;
+
+		if (isWrongPath)
+			PrintWrongDirectory();
+
+		return isWrongPath;
+	}
+
+	private void PrintStatistic()
+	{
+		var session = AppProcess.CurrentSession;
+		Console.WriteLine("Ok");
+		Console.WriteLine($"{session.Contexts.Count} contexts were processed.");
+		Console.WriteLine($"{session.Models.Count} entities were processed.");
+		Console.WriteLine($"{session.Configurations.Count} configurations were created.");
+	}
+
+	private static void PrintAskMessage()
+	{
+		Console.Clear();
+		Console.WriteLine("Put an absolute path to the root directory with contexts and models:");
+	}
+
+	private static void PrintFinalMessages()
+	{
+		Console.WriteLine();
+		Console.WriteLine("You should only do clean up the root directory");
+		
+		AskToStopOrContinue();
+	}
+
+	private void PrintWrongDirectory()
+	{
+		Console.WriteLine("The path is not a directory.");
+		Console.WriteLine("Press any key to try again.");
+		AskToStopOrContinue();
+	}
+
+	private static void AskToStopOrContinue()
+	{
+		Console.WriteLine("Press any key to do else or Esc to exit");
+		var consoleKeyInfo = Console.ReadKey();
+
+		if (consoleKeyInfo.Key is ConsoleKey.Escape)
+			Environment.Exit(0);
+	}
+}
--- EfCoreHelper/Program.cs
+++ EfCoreHelper/Program.cs
@@ -1,32 +1,6 @@
 ﻿using EfCoreHelper.App;
-using EfCoreHelper.FilePart;
 
-string path;
-bool isFile;
+var app = new AppManager();
 
-do
-{
-	Console.Clear();
-	Console.WriteLine("Give an absolute path to the root directory with contexts and models:");
-	path = Console.ReadLine()!;
-
-	isFile = string.IsNullOrWhiteSpace(path) || Directory.Exists(path) is not true;
-
-	if (isFile)
-		PrintWrongDirectory();
-} while (isFile);
-
-AppProcess.NewSession();
-var fileContext = new FileManager(path ?? throw new InvalidOperationException());
-fileContext.DoWind();
-
-Console.WriteLine("Ok");
-Console.WriteLine("You should only do clean up the root directory");
-Console.ReadKey();
-
-static void PrintWrongDirectory()
-{
-	Console.WriteLine("The path is not a directory.");
-	Console.WriteLine("Press any key to try again.");
-	Console.ReadKey();
-}
+while (true)
+	app.Run();
